0061f02560ea6623065debee2d4d2e8429f8546c
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2024-03-12 18:40:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let ans = ""
+	while(num >= 2)
+		{
+			let quotient = num%2;
+			ans = quotient+ans
+			num = parseInt(num/2);
+		}
+	if(num == 1)
+	{
+		ans = 1 + ans;
+	}
+  return ans;
 }
 
 window.decimalToBinary = decimalToBinary;
